dea44876570d5d0e800e7a8f569f8e94ba582c3b
Ayden Wong <aydenwongfs@gmail.com>
2024-04-09 22:08:31 +0800

    Fixed bug of zeros and added algorithm selection

--- app/mtp-benchmark.cpp
+++ app/mtp-benchmark.cpp
@@ -33,25 +33,40 @@ using Mtp = MatMulImpl::Multiplication;
 using tick_type = std::chrono::high_resolution_clock::duration::rep;
 
 const char *help_msg =
-    "mtp-bechmark [-h|--help] [csvOut]\n"
+    "mtp-bechmark [-h|--help] [-a|--alg algName] [csvOut]\n"
     "-h | --help\n"
     "Prints this help message and exits.\n"
+    "-a | --alg\n"
+    "Only runs test with the named algorithm. Available algorithm: naive, div_and_conquer, strassen."
     "csvOut\n"
     "Default: alg-runtimes<current datetime>.csv\n"
     "The output path of the csvFile\n";
 
-constexpr long init_batch_size = 0x1000000;  // Put a number that is 2^n here
+constexpr long init_batch_size = 0x400000;  // = 4^11
+
+const char *alg_names[] = {"naive", "div_and_conquer", "strassen"};
 
 int main(int argc, char const *argv[]) {
     std::string csvOut("");
+    std::vector<bool> used_algs = {true, true, true}; // refactor!!
     for (int i = 1; i < argc; i++) {
         std::string s(argv[i]);
         if (s == "--help" || s == "-h") {
             std::cout << help_msg << std::endl;
             return 0;
+        } else if (s == "-a" || s == "--alg") {
+            used_algs = {false, false, false};
+            std::string alg_name(argv[i+1]);
+            if (alg_name == "naive") used_algs[0] = true;
+            else if (alg_name == "div_and_conquer") used_algs[1] = true;
+            else if (alg_name == "strassen") used_algs[2] = true; // refactor later!!
+            else {
+                std::cout << "Unsupported algorithm: " << alg_name << std::endl;
+                return -1;
+            }
         } else if (s.find('-') != -1) {
             std::cout << "Unrecognized argument: " << help_msg << std::endl;
-            return 0;
+            return -1;
         } else {
             csvOut.assign(argv[i]);
         }
@@ -65,10 +80,13 @@ int main(int argc, char const *argv[]) {
     std::fstream out_file;
     out_file.open(csvOut, std::ios::out);
     out_file << "Alg_name,Entry_type,RowA,ColA,RowB,ColB,Tests\n";
-    const char *alg_names[] = {"naive", "div_and_conquer", "strassen"};
     std::cout << "Running algorithms: ";
+    int i = 0;
     for (auto &&s : alg_names) {
-        std::cout << s << ' ';
+        if (used_algs[i]) {
+            std::cout << s << ' ';
+        }
+        i++;
     }
     std::cout << '\n';
     std::cout << "Timing using std::chrono::high_resolution::clock\nResolution:"
@@ -77,10 +95,15 @@ int main(int argc, char const *argv[]) {
     int i_alg = 0;
     int i_batch = 1;
     for (auto &&f : std::initializer_list<std::function< Matrix2<double>(const Matrix2<double>&, const Matrix2<double>&)> >{&Mtp::naive<double>, &Mtp::div_and_conquer_sq2<double>, &Mtp::strassen<double>}) {
+        if (!used_algs[i_alg]) {
+            i_alg++;
+            continue;
+        }
         int matrix_size = 2;
         for (long batch_size = init_batch_size; batch_size > 0; batch_size /= 4, i_batch++, matrix_size*=2) {
             std::cout << "Batch #" << i_batch << ": Multiplying " << matrix_size << "x" << matrix_size << " matrices with " << alg_names[i_alg] << ", batch size " << batch_size << std::endl;
-            std::vector<tick_type> unit_times(batch_size);
+            std::vector<tick_type> unit_times;
+            unit_times.reserve(batch_size);
             for (long i = 0; i < batch_size; i++) {
                 // prepare matrices
                 auto m1 = MatrixGenerator<double>::random_fill(matrix_size,
